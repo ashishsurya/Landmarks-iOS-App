0304f6dc82f7e89a771b86a48e9c17246227be0c
ashishsurya <ashishsurya126@gmail.com>
2022-06-14 10:15:21 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Surya Ashish on 14/06/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
